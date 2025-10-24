13b69cb6a541239c889c480deef1d7af3de30b97
Денис Ковтун <bdjibm@bk.ru>
2025-10-24 12:43:24 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,9 +1,17 @@
 #pragma once
+#include "Generator.h"
 #include <string>
 #include <stdexcept>
 
 namespace miit::algebra
 {
+    class Generator 
+    {
+    public:
+        virtual double generate() = 0;
+        virtual ~Generator() = default;
+    };
+
     template<typename T>
     class Matrix
     {
@@ -13,29 +21,137 @@ namespace miit::algebra
         
     public:
         // Конструкторы по умолчанию
-        Matrix();
-        Matrix(size_t size);
-        Matrix(const Matrix& other);
-        Matrix(Matrix&& other) noexcept;
-        
+        Matrix() : data(nullptr), size(0) {}
+
+        Matrix(size_t size) : size(size)
+        {
+            data = new T[size];
+        }
+
+        Matrix(const Matrix& other) : size(other.size)
+        {
+            data = new T[size];
+            for (size_t i = 0; i < size; ++i)
+            {
+                data[i] = other.data[i];
+            }
+        }
+
+        Matrix(Matrix&& other) noexcept : data(other.data), size(other.size)
+        {
+            other.data = nullptr;
+            other.size = 0;
+        }
+
         // Деструктор
-        ~Matrix();
-        
+        ~Matrix()
+        {
+            delete[] data;
+        }
+
         // Операторы присваивания
-        Matrix& operator=(const Matrix& other);
-        Matrix& operator=(Matrix&& other) noexcept;
-        
+        Matrix& operator=(const Matrix& other)
+        {
+            if (this != &other)
+            {
+                delete[] data;
+                size = other.size;
+                data = new T[size];
+                for (size_t i = 0; i < size; ++i)
+                {
+                    data[i] = other.data[i];
+                }
+            }
+            return *this;
+        }
+
+        Matrix& operator=(Matrix&& other) noexcept
+        {
+            if (this != &other)
+            {
+                delete[] data;
+                data = other.data;
+                size = other.size;
+                other.data = nullptr;
+                other.size = 0;
+            }
+            return *this;
+        }
+
         // Операторы сдвига
-        Matrix operator<<(int shift) const;
-        Matrix operator>>(int shift) const;
-        
+        Matrix operator<<(int shift) const
+        {
+            Matrix result(size);
+            shift = shift % size;
+            for (size_t i = 0; i < size; ++i)
+            {
+                result.data[(i + shift) % size] = data[i];
+            }
+            return result;
+        }
+
+        Matrix operator>>(int shift) const
+        {
+            Matrix result(size);
+            shift = shift % size;
+            for (size_t i = 0; i < size; ++i)
+            {
+                result.data[i] = data[(i + shift) % size];
+            }
+            return result;
+        }
+
         // Оператор разыменования
-        T& operator[](size_t index);
-        const T& operator[](size_t index) const;
-        
+        T& operator[](size_t index)
+        {
+            if (index >= size)
+            {
+                throw std::out_of_range("Index out of range");
+            }
+            return data[index];
+        }
+
+        const T& operator[](size_t index) const
+        {
+            if (index >= size)
+            {
+                throw std::out_of_range("Index out of range");
+            }
+            return data[index];
+        }
+
         // Методы
-        std::string to_string() const;
-        size_t get_size() const;
-        void fill(Generator& generator);
+        std::string to_string() const
+        {
+            std::ostringstream oss;
+            oss << "[";
+            for (size_t i = 0; i < size; ++i)
+            {
+                oss << data[i];
+                if (i < size - 1)
+                {
+                    oss << ", ";
+                }
+            }
+            oss << "]";
+            return oss.str();
+        }
+
+        size_t get_size() const
+        {
+            return size;
+        }
+
+        void fill(Generator& generator)
+        {
+            for (size_t i = 0; i < size; ++i)
+            {
+                data[i] = static_cast<T>(generator.generate());
+            }
+        }
     };
+
+    // Явные инстанцирования для часто используемых типов
+    template class Matrix<int>;
+    template class Matrix<double>;
 }
